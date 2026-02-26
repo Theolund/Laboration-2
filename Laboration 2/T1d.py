a3481e367b5471e1043235f53c94eecbe96d9f4b
Theolund <lund.tke@gmail.com>
2026-02-26 18:16:03 +0100

    Update T1d.py

--- Laboration 2/T1d.py
+++ Laboration 2/T1d.py
@@ -18,27 +18,13 @@ def solveivp():
     sol=solve_ivp(F,tspan,Q0,method='RK45',args=(R,L,C), dense_output=True)
     return sol
 
-sol= solveivp()
+answer= solveivp()
 
 
 def q(t):
-    return sol.sol(t)[0]
+    return answer.sol(t)[0]
 
 def framatEulerSystem(F, tspan, U0, h):
-    
-    """ 
-    Implementerar Framåt Euler för ODE-System
-    
-    Parameters:
-        F       : Vektorvärd function F(t, U)
-        tspan   : Tidsintervall
-        U0      : initial state U0
-        h       : steglängd, eller tidssteg
-    
-    Returns:
-        tk     : numpy array of time points
-        Uk     : numpy array of state values
-    """
     
     n_steps = round(np.abs(tspan[1]-tspan[0])/h)
     tk = np.zeros(n_steps+1)
@@ -59,16 +45,15 @@ def main():
     R=1
     L=2
     C=0.5
-    N_values=[80,160,320,640]
+    N_values=[20,40,80,160]
     for N in N_values:
         h=(tspan[1]-tspan[0])/N
         tk,Uk=framatEulerSystem(lambda t, U:F(t,U,R,L,C),tspan,U0,h)
         plt.plot(tk,Uk[:,0],label=f'FE N={N}')
     
-    t_ref=np.linspace(0,20,1000)
+    t_ref=np.linspace(tspan[0],tspan[1],1000)
     plt.plot(t_ref,q(t_ref),label='RK45 (referens)')
     plt.legend()
     plt.grid(True)
-    plt.ylim(-6,6)
     plt.show()
 main()
